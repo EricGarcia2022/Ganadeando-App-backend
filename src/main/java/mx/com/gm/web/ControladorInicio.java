c45850c3b73f7efc2243ac20a918ad1cb61f8d8d
EricGarcia2022 <eric.garcia4147@gmail.com>
2024-10-29 17:42:49 -0500

    mi primer commit

---
+++ src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -0,0 +1,58 @@
+package mx.com.gm.web;
+
+import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
+import mx.com.gm.servicio.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var personas = personaService.listarPersonas();
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<String> guardar(@RequestBody Persona persona){
+        try {
+            personaService.guardar(persona);
+            log.info("Persona guardada con éxito");
+            return ResponseEntity.ok("Persona guardada con éxito");
+        } catch (Exception e) {
+            log.error("Error al guardar la persona", e);
+            return ResponseEntity.status(500).body("Error al guardar la persona");
+        }
+    }
+
+
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
+}
